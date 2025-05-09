da4d30c7484a828908c85770d1f4efbe34def9cd
HUAHUA768 <1783789220@qq.com>
2025-05-09 20:20:10 +0800

    函数作业讲解第二集

---
+++ 046函数作业（二）/FileName.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+	char arr[10] = { 'a','b','c' };
+	char ch1[10] = "abc";  //相当于数组放了a b c \0 0 0 0 0 0 0
+	system("pause");
+	return 0;
+}
